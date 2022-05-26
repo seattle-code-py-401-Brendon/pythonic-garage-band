efca4b970e73b3f37398bfae78671faed3b2c02a
Brendon <Brendonhampton1988@gmail.com>
2022-05-25 19:48:15 -0700

    finished bassist test and instantiation

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -20,8 +20,21 @@ class Guitarist:
     def __str__(self):
         return f"My name is {self.name} and I play guitar"
 
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+
 class Bassist:
-    pass
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
 
 class Drummer:
-    pass
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
--- tests/test_band.py
+++ tests/test_band.py
@@ -17,7 +17,7 @@ def test_guitarist_str():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_guitarist_repr():
     joan = Guitarist("Joan Jett")
     actual = repr(joan)
@@ -25,7 +25,7 @@ def test_guitarist_repr():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_drummer_str():
     sheila = Drummer("Sheila E.")
     actual = str(sheila)
@@ -33,7 +33,7 @@ def test_drummer_str():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_drummer_repr():
     sheila = Drummer("Sheila E.")
     actual = repr(sheila)
@@ -41,7 +41,7 @@ def test_drummer_repr():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_bassist_str():
     meshell = Bassist("Meshell Ndegeocello")
     actual = str(meshell)
@@ -49,7 +49,7 @@ def test_bassist_str():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_bassist_repr():
     meshell = Bassist("Meshell Ndegeocello")
     actual = repr(meshell)
