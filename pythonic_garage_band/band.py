829ace55ab3fd433aad32e99bd1b5320a866fa0b
Brendon <Brendonhampton1988@gmail.com>
2022-05-25 19:21:38 -0700

    got guitarest to pass

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -14,7 +14,11 @@ class Musician:
     pass
 
 class Guitarist:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
 
 class Bassist:
     pass
